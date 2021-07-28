cf26ae84fbe46aa176287b01856d6f8c66f2cf40
Close0 <Close0@gmail.com>
2021-07-28 16:35:00 -0400

    Update check_sex_samtools.sh

--- check_sex_samtools.sh
+++ check_sex_samtools.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 xcov=$(echo "scale=4; $(samtools idxstats $1 | grep "X" | cut -f 3)/$(samtools idxstats $1 | grep "X" | cut -f 2)" | bc)
 ycov=$(echo "scale=4; $(samtools idxstats $1 | grep "Y" | cut -f 3)/$(samtools idxstats $1 | grep "Y" | cut -f 2)" | bc)
 rat=$(echo "scale=4; ${xcov}/${ycov}" | bc)
